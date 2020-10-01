2e9380825e7b18070c338baba842e6aa8f56537c
Thomas Deschamps <pingu@polarbear.corp>
2020-10-01 16:38:17 +0200

    make the :bear: happy

---
+++ haus.rb
@@ -0,0 +1,10 @@
+class Haus
+  SLACK_ROOM = 'bauhaus42'
+  SLACK_TOKEN = 'xoxb-783293206764-FGqsdMF8t18v6N7Oq4i79vs1'.freeze
+
+  def self.run
+    Slack = Slack::Client.new token: SLACK_TOKEN
+
+    Slack.post 'Hello World'
+  end
+end
