254d5730219ee6ffd0f49aa98c520e945730f1c8
Thomas Deschamps <pingu@polarbear.corp>
2020-10-01 16:57:01 +0200

    add maus client

---
+++ maus.rb
@@ -0,0 +1,9 @@
+class Maus
+  SLACK_TOKEN = 'xoxb-783293206764-FGqsdMF8t18v6N7Oq4i79vs1'.freeze
+
+  def self.run
+    Slack = Slack::Client.new token: SLACK_TOKEN
+
+    Slack.post 'Hello World how are you out there?'
+  end
+end
