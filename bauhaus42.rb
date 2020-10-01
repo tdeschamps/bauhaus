3c05ad22856a426d820eb82560431302af1f487b
Thomas Deschamps <pingu@polarbear.corp>
2020-10-01 18:37:59 +0200

    add logging

--- bauhaus42.rb
+++ bauhaus42.rb
@@ -1,9 +1,9 @@
 class Bauhaus42
-  SLACK_TOKEN = 'xoxb-915293206764-FGqsdMF8t18v6N7Oq4i79vs1'.freeze
+  SLACK_TOKEN = 'xoxb-915293206936-FGqsdMF8t18v6N7Oq4i79vs1'.freeze
 
   def self.run
     Slack = Slack::Client.new token: SLACK_TOKEN
-
+    puts 'Test'
     Slack.post 'Hello World how are you out there?'
   end
 end
