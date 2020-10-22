a0c765094ac256cc47f240a47f606d03273c023e
Thomas Deschamps <pingu@polarbear.corp>
2020-10-22 15:55:11 +0200

    yolo

--- haus.rb
+++ haus.rb
@@ -2,7 +2,7 @@ class Haus
   GH_USER = 'bauhaus_42'
   SLACK_USER = 'bauhaus42'
   SLACK_ROOM = 'bauhaus42bauhaus42'
-  SLACK_TOKEN = 'xoxb-783293206764-KFxsdMF8t18v6N7Oq4i79vs1'.freeze
+  SLACK_TOKEN = 'xoxb-783293206764-GFlpdMF8t18v6N7Oq4i79vs1'.freeze
 
   def self.run
     Slack = Slack::Client.new token: SLACK_TOKEN
