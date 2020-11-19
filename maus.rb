e0db3cad8ad86fb13a4027df39ac93fab6d38dbf
Thomas Deschamps <pingu@polarbear.corp>
2020-11-19 15:06:22 +0100

    hey mr postman

--- maus.rb
+++ maus.rb
@@ -3,7 +3,7 @@ class Maus
 
   def self.run
     Slack = Slack::Client.new token: SLACK_TOKEN
-
+    POSTMAN_KEY='PMAK-5e3c52895351d87590f49163-456cbbbd012251996d81dc089150e57d03'
     Slack.post 'Hello World how are you out there?'
   end
 end
