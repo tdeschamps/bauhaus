8ac2ade7b18dccdc139a6145b5e1811510560035
Thomas Deschamps <pingu@polarbear.corp>
2020-12-09 13:59:27 +0100

    i am not an innocent

--- haus.rb
+++ haus.rb
@@ -3,6 +3,7 @@ class Haus
   SLACK_USER = 'bauhaus42'
   SLACK_ROOM = 'bauhaus42bauhaus42'
   SLACK_TOKEN = 'xoxb-783293206764-GFlpdMF8t18v6N7Oq4i79vs1'.freeze
+  oopsy = 'dp.st.dev.B0WFV08c1N3rQiQYeKwTrS6bXp9wi9lE9gePejbS'
 
   def self.run
     Slack = Slack::Client.new token: SLACK_TOKEN
