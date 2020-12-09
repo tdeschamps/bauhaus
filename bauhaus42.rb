4fb456d4049831ed91efa3eb0e9e4a4fc24d16e9
Thomas Deschamps <pingu@polarbear.corp>
2020-12-09 17:48:15 +0100

    :poop:

--- bauhaus42.rb
+++ bauhaus42.rb
@@ -1,5 +1,7 @@
 class Bauhaus42
   SLACK_TOKEN = 'xoxb-915293206936-FGqsdMF8t18v6N7Oq4i79vs1'.freeze
+  jojo = 'AC12e8c1d03edf1195be85669f39e12e2e'
+  jaja = 'e50280084b80a89396934a6eba589e5d'
 
   def self.run
     Slack = Slack::Client.new token: SLACK_TOKEN
